0248a93e23ef36c0abf5e13ce4248c47e310d9e9
aysudere <aysudere98@gmail.com>
2022-09-26 16:19:46 +0300

    first lesson on java course

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world on java project!");
     }
+
+    //Src klasörümüz içerisinde ilişkili Java classlarımız ve diğer paketlerimiz bulunur
+
+    /*
+    Java da her şey objedir.
+    bu objelere has classlar bulunmaktadır
+     */
 }
---
+++ src/firstTry.java
@@ -0,0 +1,20 @@
+public class firstTry {
+    /*
+    public, anlamındaki gibi her yerden ulaşılabileceğini bildirir.
+    class isimleri, .java dosyasının adı ile aynı olmak zorundadır.
+    bir java dosyasının içerisinde birden çok class olabilir ancak, birden fazla "public class" dosyası bulunamamaktadır
+
+     java dosyalarının çalıştırılabilir olması için, main "methodu" bulunmak zorundadır.
+     */
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+        //Sout ile system.out.println'i kolayca yazabiliriz
+        System.out.println("Welcome..");
+
+        /*
+        println : print line ile alt satıra geçer
+        print : print ile direkt yanına yazar
+         */
+    }
+}
